f27f5f5e6aaab867b23752fb3f1e87c3664a3f5a
hieunt21k25 <hieuntk25411e@st.uel.edu.vn>
2025-11-12 00:01:18 +0700

    Delete MyApp.py

--- MyApp.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from learn_gui.quadratic_equation.QuadraticEquation_MainWindowEx import QuadraticEquation_MainWindowEx
-
-app=QApplication([])
-my_gui=QuadraticEquation_MainWindowEx()
-my_gui.setupUi(QMainWindow())
-my_gui.showWindow()
-app.exec()
